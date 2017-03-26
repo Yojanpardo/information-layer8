aee58401d6736d47cf68f209dde75024f4f9bdf4
Jhonbeltran <jbeltranleon@gmail.com>
2017-03-25 23:04:37 -0500

    Agrego modelo Member

--- members/models.py
+++ members/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.IntegerField(unique=True)
+    address = models.CharField(max_length=255)
+    personal_skills= models.CharField(max_length=500)
+    team_skills= models.CharField(max_length=500)
+    weakness= models.CharField(max_length=500)
+    illness= models.CharField(max_length=500)
+    under_presure= models.CharField(max_length=500)
+    photo = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('id',)
