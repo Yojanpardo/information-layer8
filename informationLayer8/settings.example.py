274d12a74cefe01733ff2f7c40d887689dec4996
Jhonbeltran <jbeltranleon@gmail.com>
2017-03-25 23:37:51 -0500

    Cambio de base de datos (Paso de sqlite3 a postgresql) ejecuto nuevamente el test y funciona

--- informationLayer8/settings.example.py
+++ informationLayer8/settings.example.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'informationLayer8.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'layer8',
+        'USER': 'xxxxx',
+        'PASSWORD': 'xxxxx',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
