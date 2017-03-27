9f362871fd1ff8d328c626acc73a069fa83e7230
Jhonbeltran <jbeltranleon@gmail.com>
2017-03-27 17:55:51 -0500

    Agrego views a envents

--- events/views.py
+++ events/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from .mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy
+from .models import Event
+from .forms import EventForm
 
-# Create your views here.
+class EventCreate(LoginRequiredMixin, CreateView):
+    model = Event
+    success_url = reverse_lazy('events:home')
+    fields = ['date_start', 'date_end', 'title',
+              'description', 'location', 'timezone',
+              'organizer', 'email']
+
+class EventList(ListView):
+    model = Event
+
+class EventDetail(LoginRequiredMixin, DetailView):
+    model = Event
+
+class EventUpdate(LoginRequiredMixin, UpdateView):
+    model = Event
+    success_url = reverse_lazy('events:home')
+    fields = ['date_start', 'date_end', 'title',
+              'description', 'location', 'timezone',
+              'organizer', 'email']
+
+class EventDelete(LoginRequiredMixin, DeleteView):
+    model = Event
+    success_url = reverse_lazy('events:home')
