77b23c873f4c5c87a48ab074ca500d3785e2a2bf
Jhonbeltran <jbeltranleon@gmail.com>
2017-05-03 17:25:56 -0500

    Ya comienza a tomar forma el add calendar, faltan modificar los datos en el modelo

--- events/models.py
+++ events/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-timezones = (('(UTC-0500) America/Bogota', 'America/Bogota'),('(UTC-0800) America/Los_Angeles', 'America/Los_Angeles',))
+timezones = (('America/Bogota', 'America/Bogota'),('(UTC-0800) America/Los_Angeles', 'America/Los_Angeles',))
 
 class Event(models.Model):
     # date_start = models.DateTimeField(auto_now=False, auto_now_add=False,)
     # date_end = models.DateTimeField(auto_now=False, auto_now_add=False,)
     title = models.CharField(max_length=100)
-    date_start = models.CharField(max_length=255, default='2017-05-04 12:00:00')
+    date_start = models.DateTimeField(max_length=255)
     date_end = models.CharField(max_length=255, default='2017-05-04 12:00:00')
     description = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
