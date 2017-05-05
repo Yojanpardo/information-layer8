e30c41b63f3adad8b0810635ec148aa933c702dd
Jhonbeltran <jbeltranleon@gmail.com>
2017-05-04 23:39:05 -0500

    Corrijo hora del final del los eventos con una hora de mas respecto a la hora actual

--- events/models.py
+++ events/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from datetime import datetime
+from datetime import datetime, timedelta
 
 timezones = (('America/Bogota', 'America/Bogota'),('(UTC-0800) America/Los_Angeles', 'America/Los_Angeles',))
 
 class Event(models.Model):
-    # date_start = models.DateTimeField(auto_now=False, auto_now_add=False,)
-    # date_end = models.DateTimeField(auto_now=False, auto_now_add=False,)
+    onehour = datetime.now() + timedelta(hours=+1)
     title = models.CharField(max_length=100)
     date_start = models.DateTimeField(default=datetime.now)
-    date_end = models.DateTimeField(default=datetime.now)
+    date_end = models.DateTimeField(default=onehour)
     description = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     timezone = models.CharField(default='America/Bogota', choices=timezones, max_length=255)
