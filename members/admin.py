32663a7680eb4655f9cb9b0ff34a51cd8bcb6cb4
Jhonbeltran <jbeltranleon@gmail.com>
2017-05-10 15:38:23 -0500

    corrijo nombre de clase de event en admin

--- members/admin.py
+++ members/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Member
 
 @admin.register(Member)
-class AdminMember(admin.ModelAdmin):
+class AdminEvent(admin.ModelAdmin):
     list_display = ('name', 'last_name', 'phone', 'email', 'address',
                     'personal_skills', 'team_skills', 'weakness',
                     'under_presure',)
