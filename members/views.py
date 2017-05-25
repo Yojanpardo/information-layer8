5411555ed0201a7dd9982c54bca93983997f6109
Jhonbeltran <jbeltranleon@gmial.com>
2017-05-24 22:18:39 -0500

    Agrego librerias necesarias para generar el reporte pdf

--- members/views.py
+++ members/views.py
@@ -7,6 +7,16 @@ from .mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse_lazy
 from .models import Member
 from .forms import MemberForm
+from django.http import HttpResponse
+
+import os
+from io import BytesIO
+from reportlab.lib.pagesizes import A4, cm
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.enums import TA_CENTER
+from reportlab.lib import colors
+from reportlab.pdfgen import canvas
+from reportlab.platypus import Paragraph, Table, TableStyle, Image
 
 # class MemberCreate(LoginRequiredMixin, CreateView):
 #     model = Member
@@ -31,3 +41,6 @@ class MemberDetail(LoginRequiredMixin, DetailView):
 # class MemberDelete(LoginRequiredMixin, DeleteView):
 #     model = Member
 #     success_url = reverse_lazy('members:home')
+
+def reportPDF():
+	pass
